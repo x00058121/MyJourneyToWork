cf98c530cb0ff613039125684e859aa71a99f9de
Karen Nolan <Karen.Nolan@TUDublin.ie>
2023-11-15 13:35:22 +0000

    Changed method from private to public to allow for UnitTes creation

--- MyJourneyToWork/Calculator.cs
+++ MyJourneyToWork/Calculator.cs
@@ -53,7 +53,7 @@ namespace Calculator
         public TransportModes transportMode { get; set; }
 
         //Ensure that distance is in miles for calculation
-        private double convertDistance()
+        public double convertDistance()
         {
             if (milesOrKms.Equals(DistanceMeasurement.kms))
                 return this.distance / 1.609344;
